5ba9515a16f2b04c0155eb8712d836d2ede52bb0
Oluwaseyi1945 <Oluwaseyioladejo53@gmail.com>
2026-08-21 17:47:49 +0100

    Fix product details and URL routing

--- config/urls.py
+++ config/urls.py
@@ -16,23 +16,39 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from cart import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('cart.urls')),
-    path('api/', include('cart.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path("category/",views.category,name="category" ),
-    path( "product-details/",views.productDetails,name="product_details"),
+
+    path("admin/", admin.site.urls),
+
+    # Website pages
+    path("", include("cart.urls")),
+
+    # JWT API
+    path(
+        "api/token/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair"
+    ),
+
+    path(
+        "api/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh"
+    ),
 
 ]
 
+
 if settings.DEBUG:
+
     urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
