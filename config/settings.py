9f9edb94ccb0d5b7f5c26e5a093789753ac285b1
Oluwaseyi1945 <Oluwaseyioladejo53@gmail.com>
2026-08-21 14:08:28 +0100

    Configure production database

--- config/settings.py
+++ config/settings.py
@@ -103,11 +103,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-
 DATABASES = {
     "default": dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
+        ssl_require=bool(os.environ.get("DATABASE_URL")),
     )
 }
 
